29858606e2f23d7135b03696336c379e3a404de3
Alex_L <alexandrrbatkovich@gmail.com>
2026-09-12 16:42:25 -0700

    'true' now true

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -10,7 +10,7 @@ export async function getImagesByQuery(query, page) {
       q: query,
       image_type: 'photo',
       orientation: 'horizontal',
-      safesearch: 'true',
+      safesearch: true,
       page: page,
       per_page: 15,
     },
